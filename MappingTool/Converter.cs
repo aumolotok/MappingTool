97ea1276148fdb58543e4d0cb68eca51a1ef13e0
aumolotok <aumolotok@mail.ru>
2017-06-17 16:36:01 +0300

    Class to ClassDocs

---
+++ MappingTool/Converter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace MappingTool
+{
+   static class Converter
+    {
+        public static List<XElement> XmlEater(XDocument XDoc)
+        {
+            List<XElement> ellist = new List<XElement>();
+
+            foreach (XElement element in XDoc.Descendants())
+            {
+                if (element.Descendants().Count() == 0)
+                {
+                    ellist.Add(element);
+                }
+            }
+            return ellist;
+        }
+
+        public static void ShowXpaths(List<XElement> ellist)
+        {
+            foreach (XElement el in ellist)
+            {
+                var Array = el.AncestorsAndSelf().ToArray();
+                for (int i = Array.Length - 1; i >= 0; i--)
+                {
+                    Console.Write(Array[i].Name + @"/");
+                }
+                Console.WriteLine();
+            }
+
+        }
+
+
+
+
+
+    }
+}
--- MappingTool/Form1.cs
+++ MappingTool/Form1.cs
@@ -61,39 +61,6 @@ namespace MappingTool
         }
     }
 
-    static class Converter
-    {
-        public static List<XElement> XmlEater(XDocument XDoc)
-        {
-            List<XElement> ellist = new List<XElement>();
-
-            foreach (XElement element in XDoc.Descendants())
-                {
-                    if (element.Descendants().Count() == 0)
-                    {
-                        ellist.Add(element);
-                    }
-                }
-            return ellist;
-        }
-
-        public static void ShowXpaths(List<XElement> ellist)
-        {
-            foreach (XElement el in ellist)
-            {
-                var Array = el.AncestorsAndSelf().ToArray();
-                for (int i = Array.Length-1; i >=0; i--)
-                {
-                    Console.Write(Array[i].Name + @"/");
-                }
-                Console.WriteLine();
-            }
-
-        }
-
-
-
-    }
 }
 //    static public void WR()
 //    {
